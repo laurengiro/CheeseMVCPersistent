cc6efe43bcfe2d9d003bd796177ffdf372c2134e
Lauren Girolmetti <laurengiro@gmail.com>
2019-12-14 01:10:26 -0500

    Finished Studio 2 - Added Relationships to DB

--- src/CheeseMVC/Migrations/20191214052509_AddedDbRelationships.Designer.cs
+++ src/CheeseMVC/Migrations/20191214052509_AddedDbRelationships.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace CheeseMVC.Migrations
 {
     [DbContext(typeof(CheeseDbContext))]
-    partial class CheeseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191214052509_AddedDbRelationships")]
+    partial class AddedDbRelationships
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
